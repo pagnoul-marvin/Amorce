f64a390fd7e4ba3fe0700e94b0c1c4aa7130fc85
pagnoul-marvin <marvin.pagnoul@student.hepl.be>
2025-01-15 12:57:07 +0100

    Open and close nav for footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,6 @@
-<footer class="footer flex">
+<footer class="footer flex" x-data="{openMarginLeft: '16.8125em', closeMarginLeft: '6.50em', marginLeft: '16.8125em'}"
+        @toggle-nav.window="marginLeft = $event.detail[0].isNavVisible ? openMarginLeft : closeMarginLeft"
+        x-bind:style="{ marginLeft: marginLeft }">
 
     <p class="footer_copy hel_bold">&copy; Amorce 2025</p>
 
